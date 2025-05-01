9b29546224bb36e24632fcdccea33a92f20bf436
MoglaVOS <yurinandrew.29@gmail.com>
2025-05-01 23:53:28 +0300

    Refactor (mps): Rename app from 'api' to 'django_db_app'

--- mps/practice04/db_admin/django_db_app/apps.py
+++ mps/practice04/db_admin/django_db_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class DjangoDbAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'django_db_app'
